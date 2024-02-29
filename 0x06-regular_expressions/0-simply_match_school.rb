05f57603e6a69d6e8011481cd289458965c38dd2
Oluwatobi Alani <adebayodeboraholuwatobi@gmail.com>
2024-02-29 17:43:02 +0100

    developer files

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
 # A regular expression that must match School
 puts ARGV[0].scan(/School/).join
+
--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches a given pattern
+# A regular expression that is matches a given pattern of 10 digit phone number
 puts ARGV[0].scan(/hbt*n/).join
+
